33ff80dc406b40e1e82d54090a6def91d43d1dda
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-29 15:59:26 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-300 text-3xl font-bold  text-pink-700 underline ">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
